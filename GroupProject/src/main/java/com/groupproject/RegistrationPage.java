22f5ca9334b025bbec935ad1537fc2df5d162e75
kaito274 <huan27042003@gmail.com>
2023-04-22 15:01:00 +0700

    Adding basic components for Registration page

--- GroupProject/src/main/java/com/groupproject/RegistrationPage.java
+++ GroupProject/src/main/java/com/groupproject/RegistrationPage.java
@@ -7,11 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class RegistrationPage extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(RegistrationPage.class.getResource("RegistrationPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 450, 357);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.setResizable(false);
--- GroupProject/src/main/java/com/groupproject/RegistrationPageController.java
+++ GroupProject/src/main/java/com/groupproject/RegistrationPageController.java
@@ -3,7 +3,7 @@ package com.groupproject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class RegistrationPageController {
     @FXML
     private Label welcomeText;
 
