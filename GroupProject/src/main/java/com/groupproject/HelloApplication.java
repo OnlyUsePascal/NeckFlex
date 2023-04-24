8274f76f10d66eb735cedb74f097bffb5b9756ca
Truong Nhan <truongnhan@MBPcuaTruong6.STUSGS.RMIT.EDU.VN>
2023-04-24 09:53:14 +0700

    First commit

--- GroupProject/src/main/java/com/groupproject/HelloApplication.java
+++ GroupProject/src/main/java/com/groupproject/HelloApplication.java
@@ -2,20 +2,21 @@ package com.groupproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setResizable(false);
+        Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("hello-view.fxml"))));
+        primaryStage.setTitle("Login Page");
+        primaryStage.setScene(new Scene(root, 1600, 900));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
