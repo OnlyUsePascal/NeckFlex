222e0d2133fea64f0eb48f487eb9e428433905cc
kaito274 <huan27042003@gmail.com>
2023-04-24 10:55:28 +0700

    Update Code

--- GroupProject/src/main/java/com/groupproject/RegistrationPageController.java
+++ GroupProject/src/main/java/com/groupproject/RegistrationPageController.java
@@ -12,6 +12,8 @@ import javafx.scene.paint.Color;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static java.lang.Thread.sleep;
+
 public class RegistrationPageController implements Initializable {
 
     @FXML
@@ -35,6 +37,19 @@ public class RegistrationPageController implements Initializable {
     @FXML
     Label RegistrationPageMessage = new Label();
 
+    @FXML
+    Label requiredUsername = new Label();
+    @FXML
+    Label requiredPassword = new Label();
+    @FXML
+    Label requiredConfirmPassword = new Label();
+    @FXML
+    Label requiredFullName = new Label();
+    @FXML
+    Label requiredPhoneNumber = new Label();
+    @FXML
+    Label requiredAddress = new Label();
+
     boolean checkingPhoneNumber(String phoneNumber){
         try {
             Integer phoneNumberInteger = Integer.parseInt(phoneNumber);
@@ -48,7 +63,7 @@ public class RegistrationPageController implements Initializable {
         Platform.exit();
     }
 
-    public void onCreateButtonClick(){
+    public void onCreateButtonClick() throws InterruptedException {
 
         String username = RegistrationPageUsername.getText();
         String password = RegistrationPagePassword.getText();
@@ -56,19 +71,62 @@ public class RegistrationPageController implements Initializable {
         String fullName = RegistrationPageFullName.getText();
         String phoneNumber = RegistrationPagePhoneNumber.getText();
         String address = RegistrationPageAddress.getText();
-        System.out.println(username + " " + password + " " + confirmPassword + " " + fullName);
+
+        int errorCounter = 0;
+
+        if(username.isBlank()){
+            requiredUsername.setVisible(true);
+            errorCounter++;
+        }else{
+            requiredUsername.setVisible(false);
+        }
+
+        if(password.isBlank()){
+            requiredPassword.setVisible(true);
+            errorCounter++;
+        }else{
+            requiredPassword.setVisible(false);
+        }
+
+        if(confirmPassword.isBlank()){
+            requiredConfirmPassword.setVisible(true);
+            errorCounter++;
+        }else{
+            requiredConfirmPassword.setVisible(false);
+        }
+
+        if(fullName.isBlank()){
+            requiredFullName.setVisible(true);
+            errorCounter++;
+        }else{
+            requiredFullName.setVisible(false);
+        }
+
+        if(phoneNumber.isBlank()){
+            requiredPhoneNumber.setVisible(true);
+            errorCounter++;
+        }else{
+            requiredPhoneNumber.setVisible(false);
+        }
+
+        if(address.isBlank()){
+            requiredAddress.setVisible(true);
+            errorCounter++;
+        }else{
+            requiredAddress.setVisible(false);
+        }
 
         //ERROR CHECKING
         //Checking username
 
-        int errorCounter = 0;
+
         //Checking Password
-//        if(password.compareTo(confirmPassword) != 0){
-//            System.out.println("Password doesn't match!");
-//            RegistrationPageMessage.setText("Password doesn't match!");
-//            RegistrationPageMessage.setTextFill(Color.RED);
-//            return;
-//        }
+        if(password.compareTo(confirmPassword) != 0){
+            System.out.println("Password doesn't match!");
+            RegistrationPageMessage.setText("Password doesn't match!");
+            RegistrationPageMessage.setTextFill(Color.RED);
+            return;
+        }
 
         if(checkingPhoneNumber(phoneNumber) == false){
             RegistrationPageMessage.setText("Invalid Phone Number");
@@ -79,6 +137,7 @@ public class RegistrationPageController implements Initializable {
         if(errorCounter == 0){
             RegistrationPageMessage.setText("Register successfully!");
             RegistrationPageMessage.setTextFill(Color.GREEN);
+//            sleep(3000);
             Platform.exit();
         }
 
