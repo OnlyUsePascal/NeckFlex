1c27b76f1dd8a07ddff5e2656493c6417c02a35e
Pavel Potemkin <specialforbtsu@gmail.com>
2023-05-19 13:53:29 +0700

    rent viewer v1.1

--- GroupProject/src/main/java/com/groupproject/MakeOrderBasic.java
+++ GroupProject/src/main/java/com/groupproject/MakeOrderBasic.java
@@ -13,9 +13,16 @@ public class MakeOrderBasic extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(MakeOrderBasic.class.getResource("MakeOrderBasic.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1600 , 900);
+        MakeOrderBasicController controller = fxmlLoader.getController();
+
         stage.setTitle("Make Order");
         stage.setScene(scene);
         stage.setResizable(false);
+
+        stage.setOnCloseRequest(windowEvent -> {
+            controller.saveOrderData();
+        });
+
         stage.show();
     }
 }
--- GroupProject/src/main/java/com/groupproject/MakeOrderBasicController.java
+++ GroupProject/src/main/java/com/groupproject/MakeOrderBasicController.java
@@ -10,15 +10,15 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.time.format.ResolverStyle;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
+import java.util.Scanner;
 
 enum RentalTime {
     TWO_DAYS(Duration.ofDays(2), "Two Days"),
@@ -40,15 +40,15 @@ enum RentalTime {
 
 class Order {
     public LocalDateTime orderTime;
-    public Duration rentalTime;
+    public RentalTime rentalTime;
 
     public Order(LocalDateTime orderTime, RentalTime rentalTime) {
         this.orderTime = orderTime;
-        this.rentalTime = rentalTime.periodInSeconds;
+        this.rentalTime = rentalTime;
     }
 
     public int checkOvertime() {
-        return Duration.between(orderTime, LocalDateTime.now()).compareTo(rentalTime);
+        return Duration.between(orderTime, LocalDateTime.now()).compareTo((Duration)rentalTime.periodInSeconds);
     }
 }
 public class MakeOrderBasicController implements Initializable {
@@ -69,14 +69,10 @@ public class MakeOrderBasicController implements Initializable {
 
     private ArrayList<Order> orders = new ArrayList<>();
 
-    public void makeOrder(Event event) {
-//        Order order = new Order(LocalDateTime.now(), (RentalTime) choiceBox.getValue());
-        Order order = new Order(LocalDateTime.of(2023, 5, 14, 0, 0), (RentalTime) choiceBox.getValue());
-        orders.add(order);
-
+    private void renderOrder(Order order) {
         BiConsumer<Order, AnchorPane> cancelOrder = (o, a) -> {
             dateContainer.getChildren().remove(a);
-            orders.remove(o);
+//            orders.remove(o);
         };
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("OrderBasic.fxml"));
@@ -84,9 +80,14 @@ public class MakeOrderBasicController implements Initializable {
             AnchorPane item = fxmlLoader.load();
             OrderBasicController controller = fxmlLoader.getController();
 
-            controller.setDate(LocalDateTime.of(2023, 5, 14, 0, 0).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
-//            controller.setDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
-            controller.setType((String) choiceBox.getValue().toString());
+//            controller.setDate(LocalDateTime.of(2023, 5, 14, 0, 0).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+            controller.setDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+
+            if (choiceBox.getValue() != null)
+                controller.setType((String) choiceBox.getValue().toString());
+            else
+                controller.setType(order.rentalTime.toString());
+
             controller.setOuterOrder(order);
             controller.setOuterResponseLabel(response);
             controller.setOuterReturnOrderFunc(cancelOrder);
@@ -96,11 +97,68 @@ public class MakeOrderBasicController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public void makeOrder(Event event) {
+        Order order = new Order(LocalDateTime.now(), (RentalTime) choiceBox.getValue());
+//        Order order = new Order(LocalDateTime.of(2023, 5, 14, 0, 0), (RentalTime) choiceBox.getValue());
+        orders.add(order);
+
+        renderOrder(order);
+    }
+
+    public void loadOrderData() {
+        try {
+            File file = new File(new File("GroupProject/src/main/resources/com/data/orders.txt").getAbsolutePath());
+
+            if (file.exists()) {
+                Scanner scanner = new Scanner(file);
+
+                while (scanner.hasNextLine()) {
+                    String data = scanner.nextLine();
+                    StringTokenizer st = new StringTokenizer(data, "|");
+                    ArrayList<String> list = new ArrayList<>();
+
+                    while (st.hasMoreTokens()) {
+                        list.add(st.nextToken());
+                    }
+
+                    Order order = new Order(LocalDateTime.parse(list.get(0)), RentalTime.valueOf(list.get(1)));
+                    orders.add(order);
+                    renderOrder(order);
+                }
+            } else {
+                System.out.println("The file does not exist.");
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void saveOrderData() {
+        try {
+            File inFile = new File(new File("GroupProject/src/main/resources/com/data/orders.txt").getAbsolutePath());
+            PrintWriter printWriter = new PrintWriter(inFile);
+            for (Order o : orders) {
+                printWriter.println(o.orderTime.toString() + '|' + o.rentalTime.name());
+            }
+            printWriter.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+//        try {
+//            ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(new File("GroupProject/src/main/resources/com/data/orders.txt").getAbsolutePath()));
+//            String[] arr = (String[])inputStream.readObject();
+//            System.out.println(arr);
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+        loadOrderData();
+
         choiceBox.getItems().setAll(RentalTime.values());
         choiceBox.getSelectionModel().selectFirst();
     }
